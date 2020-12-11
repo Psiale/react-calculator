3981703d2e9ea1e66780efe910c920a24889de84
Alexis <psialedev@gmail.com>
2020-12-11 14:34:47 -0600

    finished display component

---
+++ src/components/App.js
@@ -0,0 +1,9 @@
+import ButtonPanel from './ButtonPanel';
+import Display from './display';
+
+const App = () => (
+  <div className="app">
+    <ButtonPanel />
+    <Display result="999" />
+  </div>
+);
---
+++ src/components/ButtonPanel.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ButtonPanel = () => (
+  <>
+    <div>
+      <button type="button">buton</button>
+    </div>
+  </>
+);
+
+export default ButtonPanel;
