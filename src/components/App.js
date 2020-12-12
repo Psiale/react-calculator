80ab1bc0c765f5d9d47ef284649396e8ae17e8cd
Alexis <psialedev@gmail.com>
2020-12-11 21:41:58 -0600

    replaced div with React.fragment

--- src/components/App.js
+++ src/components/App.js
@@ -2,10 +2,10 @@ import ButtonPanel from './ButtonPanel';
 import Display from './Display';
 
 const App = () => (
-  <div className="app">
+  <>
     <Display result="999" />
     <ButtonPanel />
-  </div>
+  </>
 );
 
 export default App;
