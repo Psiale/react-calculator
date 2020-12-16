259e279f67004ec7f386f5add7d4de1fab914c8d
Alexis <psialedev@gmail.com>
2020-12-16 11:29:41 -0600

    finished hooks

--- src/components/App.js
+++ src/components/App.js
@@ -4,21 +4,24 @@ import ButtonPanel from './ButtonPanel';
 import Display from './Display';
 import calculate from '../logic/calculate';
 
-const App = ({ handleClick }) => {
+const App = () => {
   const [total, setTotal] = useState(null);
   const [next, setNext] = useState(null);
   const [operation, setOperation] = useState(null);
 
+  const handleClick = buttonName => {
+    const results = calculate({ total, next, operation }, buttonName);
+
+    setTotal(results.total);
+    setNext(results.next);
+    setOperation(results.operation);
+  };
   return (
     <>
       <Display result={total || next} />
       <ButtonPanel clickHandler={handleClick} />
     </>
   );
-};
-
-App.propTypes = {
-  handleClick: PropTypes.func.isRequired,
 };
 
 export default App;
