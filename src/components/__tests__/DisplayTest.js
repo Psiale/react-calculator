8398aa9644f0d2eabba18321df3d17b135519e80
Alexis <psialedev@gmail.com>
2021-01-07 15:55:55 -0600

    finished Display  test

--- src/components/__tests__/DisplayTest.js
+++ src/components/__tests__/DisplayTest.js
@@ -5,9 +5,15 @@ import Display from '../Display';
 
 configure({ adapter: new Adapter() });
 
-describe("Describe the display component", () => {
-    it('matches the snapshot', () => {
-        const tree = renderer.create(<Display result="0" />).toJSON;
-        expect(tree).toMatchSnapshot();
-      });
+describe('Describe the display component', () => {
+  it('matches the snapshot', () => {
+    const tree = renderer.create(<Display result="0" />).toJSON;
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('displays the result parameter as the text', () => {
+    const wrapper = shallow(<Display result="10" />);
+    const text = wrapper.find('p').text();
+    expect(text).toContain('10');
+  });
 });
