33b25ca1c28a091f77956ee63289c1e4add146b1
Alexis <psialedev@gmail.com>
2021-01-08 10:32:29 -0600

    finished calculate test

--- src/components/__tests__/logic/calculateTest.js
+++ src/components/__tests__/logic/calculateTest.js
@@ -1,3 +1,25 @@
 import calculate from '../../../logic/calculate';
 
-calculate
+describe('if users press A/C key', () => {
+  it('asigns all values to null', () => {
+    expect(
+      calculate({ total: 10, next: 20, operation: '-' }, 'AC'),
+    ).toEqual({
+      total: null,
+      next: null,
+      operation: null,
+    });
+  });
+});
+
+describe('if users press = key', () => {
+  it('returns a total value with the result of the operation', () => {
+    expect(
+      calculate({ total: 10, next: 20, operation: '+' }, '='),
+    ).toEqual({
+      total: '-10',
+      next: '-20',
+      operation: '+',
+    });
+  });
+});
